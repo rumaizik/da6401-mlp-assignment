f0d8cdf939a7a6c9df788330e7f390159a122d98
Rumaiz Ibrahim K <rumaizik@gmail.com>
2026-03-08 01:42:54 +0530

    add model loading test for autograder

---
+++ src/test.py
@@ -0,0 +1,37 @@
+import numpy as np
+import argparse
+from sklearn.metrics import f1_score
+from ann.neural_network import NeuralNetwork
+
+best_config = argparse.Namespace(
+    dataset="mnist",
+    epochs=2,
+    batch_size=64,
+    loss="cross_entropy",
+    optimizer="sgd",
+    weight_decay=0.0,
+    learning_rate=0.01,
+    num_layers=2,
+    hidden_layers=[64, 64],
+    activation="relu",
+    weight_init="xavier",
+    input_size= 784,
+    output_size= 10
+
+)
+
+model = NeuralNetwork(best_config)
+
+weights = np.load("best_model.npy", allow_pickle=True).item()
+
+model.weights = weights
+
+X_test = np.random.rand(100, 784)
+
+y_true = np.random.randint(0, 10, size=(100,))
+
+y_pred = model.forward(X_test)
+
+y_pred_labels = np.argmax(y_pred, axis=1)
+
+print("F1 Score:", f1_score(y_true, y_pred_labels, average="macro"))
